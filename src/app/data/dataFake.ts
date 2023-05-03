2c3ccd6f190017adb34c5087a81973abdb59ea7c
Poliana <pollyannati@hotmail.com>
2023-05-03 14:48:17 -0300

    Adicionando dados dinamicos nos componentes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake =[
+  {
+    "id": 1,
+    "photo": "https://cdn.mos.cms.futurecdn.net/aTK8YBkBAbqCSzNGxt8adL.jpg",
+    "title": "Saiu um novo Filme",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus nostrum voluptatem veniam, explicabo, architecto magnam mollitia animi vitae perferendis, vero debitis modi doloribus eos! Repudiandae facilis fugit itaque sed."
+  },
+
+  {
+    "id": 2,
+    "photo": "https://www.jornalismo.ufv.br/cinecom/wp-content/uploads/2021/09/Critica-Viuva-Negra.jpg",
+    "title": "Saiu um novo Filme 2",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus nostrum voluptatem veniam, explicabo, architecto magnam mollitia animi vitae perferendis, vero debitis modi doloribus eos! Repudiandae facilis fugit itaque sed."
+  },
+
+  {
+    "id": 3,
+    "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/05/Personagens-de-Viuva-Negra-1024x576.jpg",
+    "title": "Saiu um novo Filme 2",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus nostrum voluptatem veniam, explicabo, architecto magnam mollitia animi vitae perferendis, vero debitis modi doloribus eos! Repudiandae facilis fugit itaque sed."
+  },
+  {
+    "id": 4,
+    "photo": "https://i0.wp.com/portrasdamagia.com.br/wp-content/uploads/2021/06/melhor-viuva-negra-1.jpeg?fit=760%2C398&ssl=1",
+    "title": "Saiu um novo Filme 2",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus nostrum voluptatem veniam, explicabo, architecto magnam mollitia animi vitae perferendis, vero debitis modi doloribus eos! Repudiandae facilis fugit itaque sed."
+  }
+
+]
